52189d0c07659e90906eab8b605af2ddf48131f0
brooke <brookejones406@gmail.com>
2021-10-29 09:55:47 -0500

    bugs fixed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -26,9 +26,9 @@ router.get('/:id', async (req, res) => {
     if(categoryData) {
       res.status(200).json(categoryData);
     } else {
-      res.status(404).json("That ish doesn't exist!");
+      res.status(404).json({message: 'No category found with this id!'});
+      return;
     }
-    
   } catch (err) {
     res.status(500).json(err);
   }
@@ -54,13 +54,24 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
   try {
-    const byeData = await Category.destroy({
+    const categoryData = await Category.destroy({
       where: {
         id: req.params.id
       }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
